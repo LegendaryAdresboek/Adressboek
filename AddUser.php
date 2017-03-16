86ea190839212fccc840db60c545c8c6f0ca90dc
Egbert-Jan <egbert-janterpstra@MacBook-Pro.local>
2017-03-16 20:40:23 +0100

    Alles om de add user goed te maken

--- AddUser.php
+++ AddUser.php
@@ -29,6 +29,8 @@ if (!$connection) {
 
 	$resultaat = mysqli_query($connection, $query);
 
+
+
 	if (!$resultaat) {
 		die ("Het is niet gelukt: ".mysqli_error($resultaat));
 	} else {
@@ -48,6 +50,37 @@ if (!$connection) {
 <title>Database invoer</title>
 <base href="/">
 <link rel="stylesheet" type="text/css" href="Homepage.css">
+
+<style>
+img{
+  width: 20px;
+  height: 20px;
+}
+
+#loginvak{
+  height: 400px;
+  width: 400px;
+  float: left;
+}
+
+#tabel
+{
+  float: left;
+  width: auto;
+}
+th{
+  width: 150px;
+}
+td{
+  width: 150px;
+}
+.checkbox{
+  width: 20px;
+}
+.icon{
+  width: 25px;
+}
+</style>
 </head>
 
 <body>
@@ -76,7 +109,51 @@ Is beheerder?: <br>
 <br>
 
 <input type="submit" value="Verzenden">
+
 </form>
-<div/>
+</div>
+
+<div id="tabel">
+
+  		<table>
+  			<tr>
+  				<th class="checkbox"><input onchange="checkAll(this)" name="chk[]" validate id="check" class="checkbox" type="checkbox"></th>
+  				<th>Voornaam</th>
+  				<th>Tussenvoegsel</th>
+  				<th>Achternaam</th>
+  				<th>Nummer</th>
+  				<th>Adress</th>
+          <th>Adress</th>
+          <th class="icon"></th>
+  				<th class="icon"></th>
+  			</tr>
+  <?php
+
+  $query3 = "SELECT * FROM gebruikers";
+  $result = mysqli_query($connection, $query3);
+
+  if(!$result)
+  {
+    echo"error";
+  }
+  		while($row = mysqli_fetch_assoc($result))
+  		{
+  		?>
+  			<tr>
+  				<td class="checkbox"><input id="check" class="checkbox" type="checkbox"></td>
+  				<td><?php print($row["voornaam"]); ?></td>
+  				<td><?php print($row['prefix']); ?></td>
+  				<td><?php print($row['achternaam']); ?></td>
+  				<td><?php print($row['gebruikersnaam']); ?></td>
+  				<td><?php print($row['wachtwoord']); ?></td>
+          <td><?php print($row['beheerder']); ?></td>
+  				<td class="icon"><button class="weizigbutton"><img src="icon.ico"></button></td>
+          <td class="icon"> <img src="trash-icon.png"></td>
+  			</tr>
+
+  <?php
+  }
+  ?>
+</div>
 </body>
 </html>
