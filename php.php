572fb2f88c73fb38e02cb82e1902be1e203dc8dd
TheBlackNarwhal <darwindewilde@gmail.com>
2017-03-21 13:07:44 +0100

    redesign

--- php.php
+++ php.php
@@ -1,20 +1,4 @@
-<!doctype html>
 
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Homepage</title>
-		<link rel="stylesheet" type="text/css" href="Homepage.css">
-		<script>
-			function errorHandler() {
-				alert("Wachtwoord en/of gebruikernaam incorrect, probeer het opnieuw.");
-				window.location.href="/"
-			}
-		</script>
-	</head>
-
-
-<body>
 	<?php
 	//Now grabs passwords from external file so no one can see.
 		$lines = file('configinlog.txt', FILE_IGNORE_NEW_LINES);
@@ -22,51 +6,5 @@
 		$conn = mysqli_connect("localhost", $lines[0], $lines[1], $lines[2]);
 		if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_errno());
-		}
-		if(!empty($_POST["name"]) && !empty($_POST["pass"])){
-	    $userName = $_POST["name"] or "";
-	    $userPass = $_POST["pass"] or "";
-			$login = false;
-		}else {
-			echo "vul wat in...";
-		}
-	    if ($userName && $userPass )
-	    {
-
-	        $query = "SELECT gebruikersnaam FROM gebruikers WHERE gebruikersnaam = '$userName' AND wachtwoord = '$userPass'";
-					$query2 = "SELECT beheerder FROM gebruikers WHERE gebruikersnaam = '$userName' AND wachtwoord = '$userPass'";
-					$admin = mysqli_query($conn, $query2);
-					$IsAdmin = mysqli_fetch_array($admin);
-	        $result = mysqli_query($conn, $query);
-	        $row = mysqli_fetch_assoc($result);
-
-	        if($row){
-	           $login = true;
-				}else {
-					$login = false;
-				}
-	}
-
-	?>
-
-	<?php
-	if ($login == true && $IsAdmin['beheerder'] == 0) {
-		$_SESSION["loggedin"] = true;
-		// stuur door naar Homepage
-		header("Location:http://adressboek.000webhostapp.com/Homepage.php");
-		exit;
-	}elseif ($login == true && $IsAdmin['beheerder'] == 1) {
-		$_SESSION["loggedin"] = true;
-		$_SESSION["IsAdmin"] = true;
-		header("Location:http://adressboek.000webhostapp.com/adminpage.php");
-		exit;
-	}
-	else {
-		echo "<script> errorHandler(); </script>";
-	}
-
-
-
+		};
  ?>
-</body>
-</html>
